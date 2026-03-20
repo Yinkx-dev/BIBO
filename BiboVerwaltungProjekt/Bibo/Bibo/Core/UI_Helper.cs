907dfe7169901b32701eba5945491f5a90c50c0e
SmileyelimS <trash4@freenet.de>
2026-03-20 05:43:32 +0100

    Sterneanzeige und Enter/Klick/Doppelklick(für datagridview) in UI_Helper ausgelagert, damit für alle Forms nutzbar

--- BiboVerwaltungProjekt/Bibo/Bibo/Core/UI_Helper.cs
+++ BiboVerwaltungProjekt/Bibo/Bibo/Core/UI_Helper.cs
@@ -4,23 +4,15 @@ using System.Windows.Forms;
 public class UI_Helper : Form
 {
     protected bool CloseApplicationOnUserClose { get; set; } = true;
-    public int Sterneanzahl { get; private set; }
 
-
-    //Schließen bei Kreuz oder Alt+F4
+    
     protected UI_Helper()
     {
         this.FormClosing += FormsGenerals_FormClosing;
     }
 
-    //Sterne von Bewertungen richtig darstellen
-    protected UI_Helper(int sterneanzahl) : this()
-    {
-        Sterneanzahl = sterneanzahl;
-    }
 
-    
-
+    //Überall Form schließen wenn Kreuz/Alt+F4
     private void FormsGenerals_FormClosing(object sender, FormClosingEventArgs e)
     {
         //Nur aufrufen, wenn schließen über kreuz oder Alt+F4, nicht bei Close()
@@ -41,5 +33,78 @@ public class UI_Helper : Form
     protected virtual bool ConfirmApplicationExit()
     {
         return true;
+    }
+
+
+    // Methode für Sternanzeige
+    public static string GetStarString(int mengeSterne)
+    {
+        int maxStars = 5;
+        string stars = "";
+
+        if(mengeSterne == 0)
+        {
+            stars = "- - - - -";
+            return stars;
+        }
+
+        for (int i = 0; i < maxStars; i++)
+        {
+            if (i < mengeSterne)
+                stars += "★";
+            else
+                stars += "☆";
+        }
+
+        return stars;
+    }
+
+
+    //Methode für Nutzen von "Enter" auf "normalem" Element des Forms
+    public static void ActionOnClickOrEnter(Control control, Action action)
+    {
+        control.Click += (s,e) => action();
+        control.KeyDown += (s, e) =>
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                e.Handled = true;
+                e.SuppressKeyPress = true;
+                action();
+            }
+        };
+    }
+
+    //Methode für Nutzen von "Enter"/Doppelklick auf Datagridview-Zeile des Forms
+    public static void ActionOnDoubleclickOrEnterDatagridview(DataGridView dgv, Action<DataGridViewRow> action)
+    {
+        //Für Enter
+        dgv.KeyDown += (s, e) =>
+        {
+            //Andere Tasten "in Ruhe lassen"
+            if(e.KeyCode != Keys.Enter)
+            {
+                return;
+            }
+
+            e.Handled = true;
+            e.SuppressKeyPress = true;
+            if(dgv.CurrentRow != null)
+            {
+                action(dgv.CurrentRow);
+            }
+        };
+
+        //Für Doppelklick auf Zelle -> Aktion für Zeile
+        dgv.CellDoubleClick += (s, e) =>
+        {
+            if(e.RowIndex >= 0)
+            {
+                //Fokus auf geklickte Zeile
+                dgv.CurrentCell = dgv.Rows[e.RowIndex].Cells[0];
+                //Aktion wie Enter, aber mit geklickter Zeile
+                action(dgv.Rows[e.RowIndex]);
+            }
+        };
     }
 }
