3a645cc83ddf54f3cdb6f77278cc870f23ccd8fd
SmileyelimS <trash4@freenet.de>
2026-05-09 13:16:07 +0200

    Abstrakte Basisklasse Medium eingeführt für theoretische zukünftige Weiterentwicklung des Programms

--- BiboVerwaltungProjekt/Bibo/Bibo/Models/Buch.cs
+++ BiboVerwaltungProjekt/Bibo/Bibo/Models/Buch.cs
@@ -4,17 +4,12 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace Bibo.Models
 {
     [Table("Buch")]
-    public class Buch
+    public class Buch : Medium
     {
         [Key]
         public int BuchID { get; set; }
+
         public string ISBN { get; set; }
-        public string Titel { get; set; }
-        public string Kurzbeschreibung { get; set; }
         public string Autor { get; set; }
-        public string Erscheinungsdatum { get; set; }
-        public string Altersgruppe { get; set; }
-        public string Genre { get; set; }
-        public bool IstAusgeliehen { get; set; }
     }
 }
---
+++ BiboVerwaltungProjekt/Bibo/Bibo/Models/Medium.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bibo.Models
+{
+    //"Gerüst" für Buch und eventuelle zukünftige andere Arten von Medien
+    public abstract class Medium
+    {
+        public string Titel { get; set; }
+        public string Kurzbeschreibung { get; set; }
+        public string Erscheinungsdatum { get; set; }
+        public string Altersgruppe { get; set; }
+        public string Genre { get; set; }
+        public bool IstAusgeliehen { get; set; }
+    }
+}
